716e481d952c2a1193cb060a1a50daec1ec3ed45
Yoghourt <449088680@qq.com>
2016-06-27 12:32:09 +0800

    修改style

--- app/src/main/java/hdenergy/mdground/com/hdenergy/activity/datareport/DataReportActivity.java
+++ app/src/main/java/hdenergy/mdground/com/hdenergy/activity/datareport/DataReportActivity.java
@@ -1,17 +1,19 @@
 package hdenergy.mdground.com.hdenergy.activity.datareport;
 
+import android.view.View;
+
 import hdenergy.mdground.com.hdenergy.R;
 import hdenergy.mdground.com.hdenergy.activity.base.ToolbarActivity;
-import hdenergy.mdground.com.hdenergy.databinding.ActivityForgetPasswordBinding;
+import hdenergy.mdground.com.hdenergy.databinding.ActivityDataReportBinding;
 
 /**
  * Created by yoghourt on 2016-06-27.
  */
-public class DataReportActivity extends ToolbarActivity<ActivityForgetPasswordBinding> {
+public class DataReportActivity extends ToolbarActivity<ActivityDataReportBinding> {
 
     @Override
     protected int getContentLayout() {
-        return R.layout.activity_forget_password;
+        return R.layout.activity_data_report;
     }
 
     @Override
@@ -25,5 +27,8 @@ public class DataReportActivity extends ToolbarActivity<ActivityForgetPasswordBi
     }
 
     //region  ACTION
+    public void nextStepAction(View view) {
+
+    }
     //endregion
 }
