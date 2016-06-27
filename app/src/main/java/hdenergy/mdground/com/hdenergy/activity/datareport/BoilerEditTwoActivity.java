7b3cdae57b5d5c229a5deb0e98a72a51f973033e
Yoghourt <449088680@qq.com>
2016-06-27 19:12:29 +0800

    数据汇报界面搭建

---
+++ app/src/main/java/hdenergy/mdground/com/hdenergy/activity/datareport/BoilerEditTwoActivity.java
@@ -0,0 +1,116 @@
+package hdenergy.mdground.com.hdenergy.activity.datareport;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import hdenergy.mdground.com.hdenergy.R;
+import hdenergy.mdground.com.hdenergy.activity.base.ToolbarActivity;
+import hdenergy.mdground.com.hdenergy.databinding.ActivityBoilerEditTwoBinding;
+import hdenergy.mdground.com.hdenergy.databinding.ItemBoilerFuelBinding;
+import hdenergy.mdground.com.hdenergy.models.Feedstock;
+import hdenergy.mdground.com.hdenergy.models.Fuel;
+
+/**
+ * Created by yoghourt on 2016-06-27.
+ */
+public class BoilerEditTwoActivity extends ToolbarActivity<ActivityBoilerEditTwoBinding> {
+
+    private EditTwoAdapter mAdapter;
+
+    private ArrayList<Fuel> mFuelArrayList = new ArrayList<>();
+
+    @Override
+    protected int getContentLayout() {
+        return R.layout.activity_boiler_edit_two;
+    }
+
+    @Override
+    protected void initData() {
+        Fuel fuel = new Fuel();
+        Feedstock feedstock = new Feedstock();
+        ArrayList<Feedstock> feedstockArrayList = new ArrayList<>();
+        feedstockArrayList.add(feedstock);
+        fuel.setFeedstocks(feedstockArrayList);
+
+        mFuelArrayList.add(fuel);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mDataBinding.recyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new EditTwoAdapter();
+        mDataBinding.recyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+
+    //region  ACTION
+    public void nextStepAction(View view) {
+
+    }
+    //endregion
+
+    class EditTwoAdapter extends RecyclerView.Adapter<EditTwoAdapter.ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_boiler_fuel, parent, false);
+            return new ViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, final int position) {
+            holder.viewDataBinding.setFuel(mFuelArrayList.get(position));
+
+            if (position == 0) {
+                holder.viewDataBinding.ivAddOrDelete.setImageResource(R.drawable.icon_addition_pay);
+
+                holder.viewDataBinding.ivAddOrDelete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mFuelArrayList.add(new Fuel());
+
+                        mAdapter.notifyDataSetChanged();
+                    }
+                });
+            } else {
+                holder.viewDataBinding.ivAddOrDelete.setImageResource(R.drawable.icon_reduce_pay);
+
+                holder.viewDataBinding.ivAddOrDelete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mFuelArrayList.remove(position);
+
+                        mAdapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return mFuelArrayList.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            public ItemBoilerFuelBinding viewDataBinding;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                viewDataBinding = DataBindingUtil.bind(itemView);
+            }
+        }
+    }
+}
--- app/src/main/java/hdenergy/mdground/com/hdenergy/activity/datareport/DataReportActivity.java
+++ app/src/main/java/hdenergy/mdground/com/hdenergy/activity/datareport/DataReportActivity.java
@@ -1,15 +1,35 @@
 package hdenergy.mdground.com.hdenergy.activity.datareport;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import hdenergy.mdground.com.hdenergy.R;
 import hdenergy.mdground.com.hdenergy.activity.base.ToolbarActivity;
 import hdenergy.mdground.com.hdenergy.databinding.ActivityDataReportBinding;
+import hdenergy.mdground.com.hdenergy.databinding.ItemBoilerBinding;
+import hdenergy.mdground.com.hdenergy.models.Boiler;
 
 /**
  * Created by yoghourt on 2016-06-27.
  */
-public class DataReportActivity extends ToolbarActivity<ActivityDataReportBinding> {
+public class DataReportActivity extends ToolbarActivity<ActivityDataReportBinding>
+        implements DatePickerDialog.OnDateSetListener {
+
+    private DatePickerDialog mBirthdayDatePickerDialog;
+
+    private DataReportAdapter mAdapter;
+
+    private ArrayList<Boiler> mBoilerArrayList = new ArrayList<>();
 
     @Override
     protected int getContentLayout() {
@@ -18,7 +38,15 @@ public class DataReportActivity extends ToolbarActivity<ActivityDataReportBindin
 
     @Override
     protected void initData() {
+        mBoilerArrayList.add(new Boiler());
+        mBoilerArrayList.add(new Boiler());
 
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mDataBinding.recyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new DataReportAdapter();
+        mDataBinding.recyclerView.setAdapter(mAdapter);
     }
 
     @Override
@@ -26,9 +54,63 @@ public class DataReportActivity extends ToolbarActivity<ActivityDataReportBindin
 
     }
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        mDataBinding.tvData.setText(year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+    }
+
     //region  ACTION
+    public void SelectDataAction(View view) {
+        if (mBirthdayDatePickerDialog == null) {
+            Calendar calendar = Calendar.getInstance();
+            mBirthdayDatePickerDialog = new DatePickerDialog(this, this, calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        }
+        mBirthdayDatePickerDialog.show();
+    }
+
     public void nextStepAction(View view) {
 
+    }
+    //endregion
+
+    //region ADAPTER
+    class DataReportAdapter extends RecyclerView.Adapter<DataReportAdapter.ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_boiler, parent, false);
+            return new ViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, final int position) {
+            holder.viewDataBinding.setBoiler(mBoilerArrayList.get(position));
+
+            holder.viewDataBinding.getRoot().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(DataReportActivity.this, BoilerEditOneActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mBoilerArrayList.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            private ItemBoilerBinding viewDataBinding;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                viewDataBinding = DataBindingUtil.bind(itemView);
+            }
+        }
     }
     //endregion
 }
