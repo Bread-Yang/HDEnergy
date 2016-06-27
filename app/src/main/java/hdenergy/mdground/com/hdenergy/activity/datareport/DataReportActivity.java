d19087e6dca02a7ec5b55cbbcde971506b4e2d74
Yoghourt <449088680@qq.com>
2016-06-27 11:13:29 +0800

    fix bgus

---
+++ app/src/main/java/hdenergy/mdground/com/hdenergy/activity/datareport/DataReportActivity.java
@@ -0,0 +1,29 @@
+package hdenergy.mdground.com.hdenergy.activity.datareport;
+
+import hdenergy.mdground.com.hdenergy.R;
+import hdenergy.mdground.com.hdenergy.activity.base.ToolbarActivity;
+import hdenergy.mdground.com.hdenergy.databinding.ActivityForgetPasswordBinding;
+
+/**
+ * Created by yoghourt on 2016-06-27.
+ */
+public class DataReportActivity extends ToolbarActivity<ActivityForgetPasswordBinding> {
+
+    @Override
+    protected int getContentLayout() {
+        return R.layout.activity_forget_password;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    //region  ACTION
+    //endregion
+}
