291a6f574611199366093e5ea222c8f9c8313af7
vic5816 <836380339@qq.com>
2016-06-28 19:13:31 +0800

    历史数据 统计

---
+++ app/src/main/java/hdenergy/mdground/com/hdenergy/activity/personalcenter/HistoryDataStaticsActivity.java
@@ -0,0 +1,26 @@
+package hdenergy.mdground.com.hdenergy.activity.personalcenter;
+
+import hdenergy.mdground.com.hdenergy.R;
+import hdenergy.mdground.com.hdenergy.activity.base.ToolbarActivity;
+import hdenergy.mdground.com.hdenergy.databinding.ActivityHistoryDatastaticsBinding;
+
+/**
+ * Created by PC on 2016-06-28.
+ */
+
+public class HistoryDataStaticsActivity extends ToolbarActivity<ActivityHistoryDatastaticsBinding>{
+    @Override
+    protected int getContentLayout() {
+        return R.layout.activity_history_datastatics;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+}
