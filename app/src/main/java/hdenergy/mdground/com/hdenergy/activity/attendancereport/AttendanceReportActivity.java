7c7d7ae2803753c86a75ec7c22c002963b847839
Yoghourt <449088680@qq.com>
2016-06-28 11:21:37 +0800

    考勤汇报

---
+++ app/src/main/java/hdenergy/mdground/com/hdenergy/activity/attendancereport/AttendanceReportActivity.java
@@ -0,0 +1,53 @@
+package hdenergy.mdground.com.hdenergy.activity.attendancereport;
+
+import android.app.DatePickerDialog;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+import hdenergy.mdground.com.hdenergy.R;
+import hdenergy.mdground.com.hdenergy.activity.base.ToolbarActivity;
+import hdenergy.mdground.com.hdenergy.databinding.ActivityAttendanceReportBinding;
+
+/**
+ * Created by yoghourt on 2016-06-28.
+ */
+public class AttendanceReportActivity extends ToolbarActivity<ActivityAttendanceReportBinding>
+        implements DatePickerDialog.OnDateSetListener {
+
+    private DatePickerDialog mBirthdayDatePickerDialog;
+
+    @Override
+    protected int getContentLayout() {
+        return R.layout.activity_attendance_report;
+    }
+
+    @Override
+    protected void initData() {
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+    }
+
+    //region  ACTION
+    public void SelectDataAction(View view) {
+        if (mBirthdayDatePickerDialog == null) {
+            Calendar calendar = Calendar.getInstance();
+            mBirthdayDatePickerDialog = new DatePickerDialog(this, this, calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        }
+        mBirthdayDatePickerDialog.show();
+    }
+
+    public void nextStepAction(View view) {
+
+    }
+    //endregion
+}
--- app/src/main/java/hdenergy/mdground/com/hdenergy/activity/homepage/RightContextFragment.java
+++ app/src/main/java/hdenergy/mdground/com/hdenergy/activity/homepage/RightContextFragment.java
@@ -11,44 +11,54 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import hdenergy.mdground.com.hdenergy.R;
+import hdenergy.mdground.com.hdenergy.activity.attendancereport.AttendanceReportActivity;
 import hdenergy.mdground.com.hdenergy.activity.datareport.DataReportActivity;
 
 /**
  * Created by PC on 2016-06-24.
  */
 
-public class RightContextFragment extends Fragment implements View.OnClickListener{
+public class RightContextFragment extends Fragment implements View.OnClickListener {
     private View mTitleBar;
     protected Toolbar mToolbar;
     protected TextView tvTitle, tvRight;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_right_context,container,false);
-        mToolbar = (Toolbar)view.findViewById(R.id.toolbars);
+        View view = inflater.inflate(R.layout.fragment_right_context, container, false);
+        mToolbar = (Toolbar) view.findViewById(R.id.toolbars);
         // 左边返回键
         mToolbar.setNavigationIcon(R.drawable.btn_return_public);
-    mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            getActivity().finish();
-        }
-    });
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
 
         // 标题
         tvTitle = ((TextView) mToolbar.findViewById(R.id.tvTitle));
-        tvTitle.setText(R.string.homePage);
-        view.findViewById(R.id.ivDateRePort).setOnClickListener(this);
+        tvTitle.setText(R.string.home_page);
+        view.findViewById(R.id.ivDateReport).setOnClickListener(this);
+        view.findViewById(R.id.ivAttendanceReport).setOnClickListener(this);
         return view;
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case  R.id.ivDateRePort:
-                Intent intent=new Intent(getActivity(), DataReportActivity.class);
+        switch (v.getId()) {
+            case R.id.ivDateReport: {
+                Intent intent = new Intent(getActivity(), DataReportActivity.class);
                 startActivity(intent);
                 break;
+            }
+
+            case R.id.ivAttendanceReport: {
+                Intent intent = new Intent(getActivity(), AttendanceReportActivity.class);
+                startActivity(intent);
+                break;
+            }
         }
     }
 }
